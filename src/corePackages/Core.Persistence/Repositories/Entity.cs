9dccf700f1c37766137c1f240eed394388b03d9e
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-10-10 19:02:58 +0300

    Added entity created date and updated date

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -3,9 +3,12 @@
 public class Entity
 {
     public int Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
 
     public Entity()
     {
+        
     }
 
     public Entity(int id) : this()
