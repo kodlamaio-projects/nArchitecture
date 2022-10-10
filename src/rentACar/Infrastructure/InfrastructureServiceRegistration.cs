61d4412ffc4e922f2232607cfc27b95ed499732f
Fatih SAĞLAM <fatih.sglmm@gmail.com>
2022-10-11 00:49:47 +0300

    feat(Core.CrossCuttingConcerns): Add-Global-Error-Logging

--- src/rentACar/Infrastructure/InfrastructureServiceRegistration.cs
+++ src/rentACar/Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Services.FindeksService;
 using Application.Services.ImageService;
 using Application.Services.POSService;
+using Core.CrossCuttingConcerns.Logging.Serilog;
+using Core.CrossCuttingConcerns.Logging.Serilog.Logger;
 using Infrastructure.Adapters.FakeFindeksService;
 using Infrastructure.Adapters.FakePOSService;
 using Infrastructure.Adapters.ImageService;
@@ -13,6 +15,7 @@ public static class InfrastructureServiceRegistration
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
         services.AddScoped<IFindeksService, FakeFindeksServiceAdapter>();
+        services.AddScoped<LoggerServiceBase, FileLogger>();
         services.AddScoped<IPOSService, FakePOSServiceAdapter>();
         services.AddScoped<IImageService, CloudinaryImageServiceAdapter>();
         return services;
