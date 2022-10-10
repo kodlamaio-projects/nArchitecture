a9dcc8bb860427305e2ae287863b52421b9fb916
torukobyte <torukobyte@gmail.com>
2022-10-10 18:25:38 +0300

    fix(Application/Service) : Possible null reference return

--- src/rentACar/Application/Services/UserService/IUserService.cs
+++ src/rentACar/Application/Services/UserService/IUserService.cs
@@ -5,6 +5,6 @@ namespace Application.Services.UserService;
 public interface IUserService
 {
     public Task<User?> GetByEmail(string email);
-    public Task<User> GetById(int id);
+    public Task<User?> GetById(int id);
     public Task<User> Update(User user);
 }
