cde34bd121eac99d25af83f086750968066c1bbd
TURAN EDİZ SAÇAKLI <turanedizsacakli@yahoo.com.tr>
2022-10-10 20:39:40 +0300

    controllerAndRepository...

---
+++ src/rentACar/Application/Services/Repositories/ISpeedRepository .cs
@@ -0,0 +1,8 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface ISpeedRepository : IAsyncRepository<Speed>, IRepository<Speed>
+{
+}
---
+++ src/rentACar/WebAPI/Controllers/SpeedsController .cs
@@ -0,0 +1,52 @@
+﻿using Application.Features.Speeds.Commands.CreateSpeed;
+using Application.Features.Speeds.Commands.DeleteSpeed;
+using Application.Features.Speeds.Commands.UpdateSpeed;
+using Application.Features.Speeds.Dtos;
+using Application.Features.Speeds.Models;
+using Application.Features.Speeds.Queries.GetByIdSpeed;
+using Application.Features.Speeds.Queries.GetListSpeed;
+using Core.Application.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SpeedsController : BaseController
+{
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdSpeedQuery getByIdSpeedQuery)
+    {
+        SpeedDto result = await Mediator.Send(getByIdSpeedQuery);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListSpeedQuery getListSpeedQuery = new() { PageRequest = pageRequest };
+        SpeedListModel result = await Mediator.Send(getListSpeedQuery);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateSpeedCommand createSpeedCommand)
+    {
+        CreatedSpeedDto result = await Mediator.Send(createSpeedCommand);
+        return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateSpeedCommand updateSpeedCommand)
+    {
+        UpdatedSpeedDto result = await Mediator.Send(updateSpeedCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteSpeedCommand deleteSpeedCommand)
+    {
+        DeletedSpeedDto result = await Mediator.Send(deleteSpeedCommand);
+        return Ok(result);
+    }
+}
