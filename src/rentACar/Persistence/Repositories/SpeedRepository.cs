c477478a6ed0ecdd48a283226cc9d3fa80249e90
TURAN EDİZ SAÇAKLI <turanedizsacakli@yahoo.com.tr>
2022-10-10 20:32:58 +0300

    Create SpeedRepository.cs

---
+++ src/rentACar/Persistence/Repositories/SpeedRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class SpeedRepository : EfRepositoryBase<Speed, BaseDbContext>, ISpeedRepository
+{
+    public SpeedRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
