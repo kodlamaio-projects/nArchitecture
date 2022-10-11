ccf818ff55091b228dec9450925f2f202be32015
uluserdar <serdar.uludag@hotmail.com>
2022-10-11 13:39:35 +0300

    refactor(Application/Features/Cars): Removed magic strings in Business Exception messages

---
+++ src/rentACar/Application/Features/Cars/Constants/CarExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Features.Cars.Constants
+{
+    public static class CarExceptionMessages
+    {
+        public static string CarNotExistsMessage => "Car not exists.";
+        public static string CarCanNotBeMaintainWhenIsRentedMessage => "Car can't be maintain when is rented.";
+        public static string CarCanNotBeMaintainWhenIsMaintenanceMessage => "Car can not be rent when is in maintenance.";
+        public static string CarCanNotBeRentWhenIsInsertedMessage => "Car can not be rent when is rented.";
+    }
+}
