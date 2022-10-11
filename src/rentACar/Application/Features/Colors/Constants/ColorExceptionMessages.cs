119ce10f530480380edf2c2a93ba4eac094c6e54
uluserdar <serdar.uludag@hotmail.com>
2022-10-11 13:46:16 +0300

    refactor(Application/Features/Colors): Removed magic strings in Business Exception messages

---
+++ src/rentACar/Application/Features/Colors/Constants/ColorExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Colors.Constants
+{
+    public static class ColorExceptionMessages
+    {
+        public static string ColorNotExistsMessage => "Color not exists.";
+        public static string ColorNameExistsMessage => "Color name exists.";
+    }
+}
--- src/rentACar/Application/Features/Colors/Rules/ColorBusinessRules.cs
+++ src/rentACar/Application/Features/Colors/Rules/ColorBusinessRules.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Colors.Constants;
+using Application.Services.Repositories;
 using Core.CrossCuttingConcerns.Exceptions;
 using Core.Persistence.Paging;
 using Domain.Entities;
@@ -17,12 +18,12 @@ public class ColorBusinessRules
     public async Task ColorIdShouldExistWhenSelected(int id)
     {
         Color? result = await _colorRepository.GetAsync(b => b.Id == id);
-        if (result == null) throw new BusinessException("Color not exists.");
+        if (result == null) throw new BusinessException(ColorExceptionMessages.ColorNotExistsMessage);
     }
 
     public async Task ColorNameCanNotBeDuplicatedWhenInserted(string name)
     {
         IPaginate<Color> result = await _colorRepository.GetListAsync(b => b.Name == name);
-        if (result.Items.Any()) throw new BusinessException("Color name exists.");
+        if (result.Items.Any()) throw new BusinessException(ColorExceptionMessages.ColorNameExistsMessage);
     }
 }
