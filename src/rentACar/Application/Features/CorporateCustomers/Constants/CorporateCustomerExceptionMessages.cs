7bf4565b757bfc91bd844017097abd574b197b45
uluserdar <serdar.uludag@hotmail.com>
2022-10-11 13:54:40 +0300

    refactor(Application/Features/CorporateCustomers): Removed magic strings in Business Exception messages

---
+++ src/rentACar/Application/Features/CorporateCustomers/Constants/CorporateCustomerExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.CorporateCustomers.Constants
+{
+    public static class CorporateCustomerExceptionMessages
+    {
+        public static string CorporateCustomerNotExistsMessage => "CorporateCustomer not exists.";
+        public static string CorporateCustomerTaxNoExistsMessage => "Corporate customer tax no already exists.";
+    }
+}
