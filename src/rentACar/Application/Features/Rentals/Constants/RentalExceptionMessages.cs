2dd5aa67fa522d345ab7cde7705a1b51e94c9d52
uluserdar <serdar.uludag@hotmail.com>
2022-10-11 15:04:00 +0300

    refactor(Application/Features/Rentals): Removed magic strings in Business Exception messages

---
+++ src/rentACar/Application/Features/Rentals/Constants/RentalExceptionMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Rentals.Constants
+{
+    public static class RentalExceptionMessages
+    {
+        public static string RentalNotExistsMessage => "Rental not exists.";
+        public static string RentalAnotherRentedCarForTheDateMessage => "Rental can't be updated when there is another rented car for the date.";
+        public static string RentalCanNotBeCreatedScorLowerMessage => "Rental can not be created when customer findeks credit score lower than car min findeks score.";
+    }
+}
--- src/rentACar/Application/Features/Rentals/Rules/RentalBusinessRules.cs
+++ src/rentACar/Application/Features/Rentals/Rules/RentalBusinessRules.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Rentals.Constants;
+using Application.Services.Repositories;
 using Core.CrossCuttingConcerns.Exceptions;
 using Core.Persistence.Paging;
 using Domain.Entities;
@@ -17,7 +18,7 @@ public class RentalBusinessRules
     public async Task RentalIdShouldExistWhenSelected(int id)
     {
         Rental? result = await _rentalRepository.GetAsync(b => b.Id == id);
-        if (result == null) throw new BusinessException("Rental not exists.");
+        if (result == null) throw new BusinessException(RentalExceptionMessages.RentalNotExistsMessage);
     }
 
     public async Task RentalCanNotBeUpdateWhenThereIsARentedCarInDate(int id, int carId, DateTime rentStartDate,
@@ -28,15 +29,14 @@ public class RentalBusinessRules
                                              r.RentEndDate >= rentStartDate &&
                                              r.RentStartDate <= rentEndDate);
         if (rentals.Items.Any())
-            throw new BusinessException("Rental can't be updated when there is another rented car for the date.");
+            throw new BusinessException(RentalExceptionMessages.RentalAnotherRentedCarForTheDateMessage);
     }
 
     public Task RentalCanNotBeCreatedWhenCustomerFindeksScoreLowerThanCarMinFindeksScore(
         short customerFindeksCreditRate, short carMinFindeksCreditRate)
     {
         if (customerFindeksCreditRate < carMinFindeksCreditRate)
-            throw new BusinessException(
-                "Rental can not be created when customer findeks credit score lower than car min findeks score.");
+            throw new BusinessException(RentalExceptionMessages.RentalCanNotBeCreatedScorLowerMessage);
         return Task.CompletedTask;
     }
 }
