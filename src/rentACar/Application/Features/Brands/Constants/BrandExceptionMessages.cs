eeafbe817a124f78c0fcc20f395b4b54dd4acacb
uluserdar <serdar.uludag@hotmail.com>
2022-10-11 13:19:33 +0300

    refactor(Application/Features/Brands): Removed magic strings in Business Exception messages

---
+++ src/rentACar/Application/Features/Brands/Constants/BrandExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Features.Brands.Constants
+{
+    public static class BrandExceptionMessages
+    {
+        public static string BrandNotExistsMessage => "Brand not exists.";
+        public static string BrandNameExistsMessage => "Brand name exists.";
+    }
+}
