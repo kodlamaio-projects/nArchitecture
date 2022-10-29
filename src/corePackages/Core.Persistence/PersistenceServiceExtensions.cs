05d57b6c053536222cf155d3077a4890e12ac31a
Yusuf Çırak <yusuf10901@hotmail.com>
2022-10-29 14:43:58 +0300

    feat(core.persistence): repository registrations with reflection

---
+++ src/corePackages/Core.Persistence/PersistenceServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Core.Persistence
+{
+    public static class PersistenceServiceExtensions
+    {
+        public static IServiceCollection AddRepositoryServices(this IServiceCollection services, Assembly assembly,
+            Type type, string interfacePrefix="I")
+        {
+            Type[] repositories = assembly.GetTypes()
+                .Where(t => t.BaseType!.IsGenericType && t.BaseType.GetGenericTypeDefinition()==type).ToArray();
+
+            foreach (Type repository in repositories)
+            {
+                string repositoryName = repository.Name;
+                
+                Type? repositoryInterface =
+                    repository.GetInterfaces().FirstOrDefault(e => e.Name == $"{interfacePrefix}{repositoryName}");
+
+                if (repositoryInterface==null)
+                {
+                    throw new NotFoundException($"Reflection could not find the interface '{interfacePrefix}{repositoryName}' for '{repositoryName}'");
+                }
+                services.AddScoped(repositoryInterface, repository);
+            }
+
+            return services;
+        }
+    }
+}
--- src/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,8 @@
-﻿using Application.Services.Repositories;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Application.Services.Repositories;
+using Core.Persistence;
+using Core.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,29 +19,10 @@ public static class PersistenceServiceRegistration
         services.AddDbContext<BaseDbContext>(options =>
                                                  options.UseSqlServer(
                                                      configuration.GetConnectionString("RentACarConnectionString")));
-        services.AddScoped<IAdditionalServiceRepository, AdditionalServiceRepository>();
-        services.AddScoped<IBrandRepository, BrandRepository>();
-        services.AddScoped<ICarRepository, CarRepository>();
-        services.AddScoped<ICarDamageRepository, CarDamageRepository>();
-        services.AddScoped<IColorRepository, ColorRepository>();
-        services.AddScoped<ICorporateCustomerRepository, CorporateCustomerRepository>();
-        services.AddScoped<ICustomerRepository, CustomerRepository>();
-        services.AddScoped<IEmailAuthenticatorRepository, EmailAuthenticatorRepository>();
-        services.AddScoped<IFindeksCreditRateRepository, FindeksCreditRateRepository>();
-        services.AddScoped<IFuelRepository, FuelRepository>();
-        services.AddScoped<IIndividualCustomerRepository, IndividualCustomerRepository>();
-        services.AddScoped<IInvoiceRepository, InvoiceRepository>();
-        services.AddScoped<IModelRepository, ModelRepository>();
-        services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
-        services.AddScoped<IOtpAuthenticatorRepository, OtpAuthenticatorRepository>();
-        services.AddScoped<IRentalRepository, RentalRepository>();
-        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-        services.AddScoped<IRentalsAdditionalServiceRepository, RentalsAdditionalServiceRepository>();
-        services.AddScoped<IRentalBranchRepository, RentalBranchRepository>();
-        services.AddScoped<ITransmissionRepository, TransmissionRepository>();
-        services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
+
+        services.AddRepositoryServices(Assembly.GetExecutingAssembly(), typeof(EfRepositoryBase<,>));
 
         return services;
     }
+
 }
