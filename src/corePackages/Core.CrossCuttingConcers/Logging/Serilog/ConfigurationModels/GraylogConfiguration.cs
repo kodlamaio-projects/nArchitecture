c469e57f3115348f8d3e6198b9a638281572bcce
uluserdar <serdar.uludag@hotmail.com>
2022-11-02 15:43:48 +0300

    feat(Core.CrossCuttingConcerns/Logging): Added GraylogLogger

---
+++ src/corePackages/Core.CrossCuttingConcers/Logging/Serilog/ConfigurationModels/GraylogConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels
+{
+    public class GraylogConfiguration
+    {
+        public string? HostnameOrAddress { get; set; }
+        public int Port { get; set; }
+    }
+}
