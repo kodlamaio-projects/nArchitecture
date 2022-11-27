ba3d363aeded2043223b963d3ee071f47cbe6883
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2022-11-27 20:20:53 +0300

    fix(core): make UpdatedDate as nullable

--- src/corePackages/Core.Persistence/Repositories/Entity.cs
+++ src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -4,7 +4,7 @@ public class Entity
 {
     public int Id { get; set; }
     public DateTime CreatedDate { get; set; }
-    public DateTime UpdatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 
     public Entity()
     {
