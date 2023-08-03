e329893bbe933362c8ac2b9b08d519c129e1ed72
furkanaltaca <furkanaltaca@gmail.com>
2023-08-03 22:18:29 +0300

    feat(db): create InMemory and MsSql DbContext

---
+++ src/starterProject/Persistence/Contexts/InMemoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts;
+public class InMemoryDbContext : BaseDbContext
+{
+    public InMemoryDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions, configuration)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            base.OnConfiguring(
+                optionsBuilder.UseInMemoryDatabase("InMemoryDb"));
+    }
+}
---
+++ src/starterProject/Persistence/Contexts/MsSqlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence.Contexts;
+public class MsSqlDbContext : BaseDbContext
+{
+    public MsSqlDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions, configuration)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+            base.OnConfiguring(
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("MsSql")));
+    }
+}
