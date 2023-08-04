e125603367a47bce83b9ee04e96e8405372a303b
furkanaltaca <furkanaltaca@gmail.com>
2023-08-05 02:19:17 +0300

    feat(businessRules): edit opeartion claim business rules and add translate service support

--- src/starterProject/Application/Features/OperationClaims/Rules/OperationClaimBusinessRules.cs
+++ src/starterProject/Application/Features/OperationClaims/Rules/OperationClaimBusinessRules.cs
@@ -1,5 +1,6 @@
 using Application.Features.OperationClaims.Constants;
 using Application.Services.Repositories;
+using Application.Services.TranslateService;
 using Core.Application.Rules;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Core.Security.Entities;
@@ -9,37 +10,46 @@ namespace Application.Features.OperationClaims.Rules;
 public class OperationClaimBusinessRules : BaseBusinessRules
 {
     private readonly IOperationClaimRepository _operationClaimRepository;
+    private readonly ITranslateService _translateService;
 
-    public OperationClaimBusinessRules(IOperationClaimRepository operationClaimRepository)
+    public OperationClaimBusinessRules(
+        IOperationClaimRepository operationClaimRepository,
+        ITranslateService translateService
+        )
     {
         _operationClaimRepository = operationClaimRepository;
+        _translateService = translateService;
     }
 
-    public Task OperationClaimShouldExistWhenSelected(OperationClaim? operationClaim)
+    public override async Task ThrowBusinessException(string message)
+    {
+        string translatedMessage = await _translateService.TranslateAsync(message);
+        await base.ThrowBusinessException(translatedMessage);
+    }
+    public async Task OperationClaimShouldExistWhenSelected(OperationClaim? operationClaim)
     {
         if (operationClaim == null)
-            throw new BusinessException(OperationClaimsMessages.NotExists);
-        return Task.CompletedTask;
+            await ThrowBusinessException(OperationClaimsMessages.NotExists);
     }
 
     public async Task OperationClaimIdShouldExistWhenSelected(int id)
     {
         bool doesExist = await _operationClaimRepository.AnyAsync(predicate: b => b.Id == id, enableTracking: false);
         if (doesExist)
-            throw new BusinessException(OperationClaimsMessages.NotExists);
+            await ThrowBusinessException(OperationClaimsMessages.NotExists);
     }
 
     public async Task OperationClaimNameShouldNotExistWhenCreating(string name)
     {
         bool doesExist = await _operationClaimRepository.AnyAsync(predicate: b => b.Name == name, enableTracking: false);
         if (doesExist)
-            throw new BusinessException(OperationClaimsMessages.AlreadyExists);
+            await ThrowBusinessException(OperationClaimsMessages.AlreadyExists);
     }
 
     public async Task OperationClaimNameShouldNotExistWhenUpdating(int id, string name)
     {
         bool doesExist = await _operationClaimRepository.AnyAsync(predicate: b => b.Id != id && b.Name == name, enableTracking: false);
         if (doesExist)
-            throw new BusinessException(OperationClaimsMessages.AlreadyExists);
+            await ThrowBusinessException(OperationClaimsMessages.AlreadyExists);
     }
 }
